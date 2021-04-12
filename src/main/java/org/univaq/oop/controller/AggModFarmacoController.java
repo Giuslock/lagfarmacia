b8566fdcf4b86beb914157e1e404cf64b429d87d
antc95 <65723704+antc95@users.noreply.github.com>
2021-04-12 04:55:43 +0200

    modifiche viste e controller
    modificata vista e controller da implementare stamattina buisness factory

--- src/main/java/org/univaq/oop/controller/AggModFarmacoController.java
+++ src/main/java/org/univaq/oop/controller/AggModFarmacoController.java
@@ -1,4 +1,95 @@
 package org.univaq.oop.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import org.univaq.oop.business.BusinessException;
+import org.univaq.oop.business.MedicineService;
+import org.univaq.oop.domain.Medicine;
+import org.univaq.oop.view.ViewDispatcher;
+import org.univaq.oop.view.ViewException;
 
-public class AggModFarmacoController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AggModFarmacoController implements Initializable, DataInitializable <Medicine> {
+    @FXML
+    private TextField nametext;
+
+    @FXML
+    private TextField descriptiontext;
+
+
+    @FXML
+    private TextField mimimumtext;
+
+    @FXML
+    private TextField quantitytext
+
+    @FXML
+    private TextField outofstocktext;
+
+    @FXML
+    private Button salvaButton;
+
+    @FXML
+    private Button eliminaButton;
+
+
+    private Medicine medicine;
+    private ViewDispatcher dispatcher;
+    private MedicineService medicineService;
+
+    public AggModFarmacoController() throws BusinessException {
+        dispatcher = ViewDispatcher.getInstance();
+        //farmacoService = new RAMFarmacoServiceImpl();
+        FarmasoftBusinessFactory factory = FarmasoftBusinessFactory.getInstance();
+        farmacoService = factory.getFarmacoService();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+
+    //vengono caricati i dati dei farmaci nelle textfield
+    @Override
+    public void initializeData(Medicine medicine) {
+        this.medicine = medicine;
+        this.nametext.setText(medicine.getName());
+        this.descriptiontext.setText(medicine.getDescription());
+
+        this.quantitytext.setText(medicine.getMinimum());
+        this.mimimumtext.setText(String.valueOf(medicine.getMinimum()));
+        this.outofstocktext.setText(String.valueOf(medicine.getMedicineStatus()));
+    }
+
+
+    //al click del tasto salva
+    @FXML
+    public void salvaAction(ActionEvent event) throws ViewException {
+        try {
+            medicine.setName(nametext.getText());
+            medicine.setDescription(descriptiontext.getText());
+
+            medicine.setQuantity(quantitytext.getText());
+            medicine.setMinimum(Integer.parseInt(mimimumtext.getText()));
+            medicine.setOutOfStock(Integer.parseInt(outofstocktext.getText()));
+
+            if( medicine.getId() == null) {
+                MedicineService.addFarmaco(medicine);
+            }
+            else {
+                medicineService.updateFarmaco(medicine);
+            }
+        } catch (BusinessException e) {
+            dispatcher.renderError(e);
+        }
+    }
+
+    @FXML
+    public void eliminaAction(ActionEvent event) throws BusinessException {
+        int id  = medicine.getId();
+        medicineService.deleteFarmaco(id);
+    }
+}
+
 }
